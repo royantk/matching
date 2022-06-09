8d6e710a84e134f9bb9f2f7cf924af5f27dcfbe9
royantk <killianr479@gmail.com>
2022-06-09 10:12:45 +0200

    Ajout afichage, tri par date, code commenté

--- matching.py
+++ matching.py
@@ -6,39 +6,30 @@
 ══════════════════════════
 '''
 
-#####################
-##     Modules     ##
-#####################
+###################################
+##     Modules et programmes     ##
+###################################
 
 from copy import deepcopy
 import json
-# from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
 
 def compatible(artist, slot):
     return artist["level"] >= slot["level"] and artist["category"] in slot["category"]
 
-''' 
-ATTENTION : AJOUTER :
-    "category" : "HF"
-    "level" : 0
-AUX CRÉNEAUX SANS CATÉGORIE/NIVEAU 
-'''
 
 ################################
 ##     Import des données     ##
 ################################
-
 
 parameters = open("/Users/kiki/Documents/Algo Matching/matching/Comedy-Club-line-up.json").read()
 
 slots = json.loads(str(parameters))["slots"]
 artists = json.loads(str(parameters))["artists"]
 
+
 ###################################
 ##     Création des créneaux     ##
 ###################################
-
-print(slots["slot1"]["weeks"][2])
 
 liste_slots = []
 for slot in slots:
@@ -49,9 +40,8 @@ for slot in slots:
             slots[slot]["artists"] = []
             liste_slots.append(deepcopy(slots[slot]))
 
-print(liste_slots)
+liste_slots.sort(key=lambda x: x["date"])   # Tri par date
 
-'(TRIER LES CRÉNEAUX PAR ORDRE CHRONOLOGIQUE ?)'
 
 ##################################################
 ##     Création et classement des comédiens     ##
@@ -60,31 +50,38 @@ print(liste_slots)
 liste_artists = []
 for x in artists:
     artists[x]["slots"] = []
-    liste_artists.append([len(liste_slots), artists[x]["level"], x, artists[x]])
+    liste_artists.append(
+        [len(liste_slots), artists[x]["level"], x, artists[x]])
 
-liste_artists.sort(reverse = True)
+liste_artists.sort(reverse=True)   # Tri par nombre de scènes croissant puis niveau décroissant
+
 
 #######################################
 ##     Assignation des comédiens     ##
 #######################################
 
 for slot in liste_slots:
-    i, j = 0, 0
+    i, j = 0, 0    # i : nombre de personnes actuellement sur le créneau, j : j-ième comédien dans la file
     while j < len(liste_artists) and i < slot["capacity"]:
-        if compatible(liste_artists[j][3],slot):
+        if compatible(liste_artists[j][3], slot):
             slot["artists"].append(liste_artists[j][3]["name"])
             liste_artists[j][3]["slots"].append(slot)
             liste_artists[j][0] -= 1
             i += 1
-            print(i,j)
         j += 1
-    liste_artists.sort(reverse = True)
-
-print(liste_slots)
+    liste_artists.sort(reverse=True)   # Tri de la file d'attente avant chaque nouveau créneau
 
 
-############################################################
-##     Résolution du problème d'optimisation linéaire     ##
-############################################################
+################################
+##     Affichage créneaux     ##
+################################
 
-# prob = LpProblem("Matching", LpMaximize)
+print("\nAttribution des créneaux :\n")
+for slot in liste_slots:
+    print(
+        "Jour n°" + str(slot["date"])
+        + " à " + slot["hour"]
+        + " (" + str(len(slot["artists"])) +
+        "/" + str(slot["capacity"]) + ") : "
+        + str(slot["artists"]).replace("[", "").replace("]","").replace("'", "")
+    )
